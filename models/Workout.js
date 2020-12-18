58dfd446dda4634443f3d96b698eb31170712e1b
eaclumpkens <eaclumpkens@gmail.com>
2020-12-18 13:54:06 -0500

    schema syntax

--- models/Workout.js
+++ models/Workout.js
@@ -33,5 +33,5 @@ const workoutSchema = new Schema ({
     ]
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 module.exports = Workout;
